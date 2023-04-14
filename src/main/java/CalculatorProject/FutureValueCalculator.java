3a7316731ec68ef1c2d0c5e6193b61b7a297d46b
isaiah vang <vang.isaiah3@gmail.com>
2023-04-14 16:06:52 -0400

    final commit (for real)

--- src/main/java/CalculatorProject/FutureValueCalculator.java
+++ src/main/java/CalculatorProject/FutureValueCalculator.java
@@ -11,7 +11,7 @@ public class FutureValueCalculator {
         double daysInTerm = Math.pow(interestDividedByDays, (365 * 5));
         double compoundInterest = principal * daysInTerm;
 
-        System.out.printf("The ending amount would be $%.2f." , compInt);
-        System.out.printf(" You would receive $%.2f in interest." , (compInt - principal));
+        System.out.printf("The ending amount would be $%.2f." , compoundInterest);
+        System.out.printf(" You would receive $%.2f in interest." , (compoundInterest - principal));
     }
 }
