57f86418a172293e53906f422e4e264a27923b84
isaiah vang <vang.isaiah3@gmail.com>
2023-04-14 15:08:12 -0400

    added some variables C2

--- src/main/java/CalculatorProject/FutureValueCalculator.java
+++ src/main/java/CalculatorProject/FutureValueCalculator.java
@@ -3,5 +3,10 @@ package CalculatorProject;
 public class FutureValueCalculator {
     public static void main(String[] args){
 
+        double deposit = 1000;
+        double intRate = 1.75;
+        double years = 5;
+
+
     }
 }
