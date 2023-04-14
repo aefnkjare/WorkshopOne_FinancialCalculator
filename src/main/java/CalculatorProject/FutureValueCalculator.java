42a2593b93bd7440fb148db92d1ee9973efa71ba
isaiah vang <vang.isaiah3@gmail.com>
2023-04-14 16:06:05 -0400

    final commit

--- src/main/java/CalculatorProject/FutureValueCalculator.java
+++ src/main/java/CalculatorProject/FutureValueCalculator.java
@@ -4,14 +4,14 @@ public class FutureValueCalculator {
     public static void main(String[] args){
 
         double principal = 1000;
-        double Interest = 0.0175;
-        double years = 5;
+        double interestRate = 0.0175;
+        double timeInYears = 5;
 
-        double rateOfInterest = 1 + Interest / 365;
-        double sumOne = Math.pow(rateOfInterest, (365 * 5));
-        double sumTwo = principal * sumOne;
+        double interestDividedByDays = 1 + interestRate / 365;
+        double daysInTerm = Math.pow(interestDividedByDays, (365 * 5));
+        double compoundInterest = principal * daysInTerm;
 
-        System.out.printf("The ending amount would be $%.2f." , sumTwo);
-        System.out.printf(" You would receive $%.2f in interest." , (sumTwo - principal));
+        System.out.printf("The ending amount would be $%.2f." , compInt);
+        System.out.printf(" You would receive $%.2f in interest." , (compInt - principal));
     }
 }
